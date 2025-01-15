b9ad28bd4b2552020c52be478515dea88a30955c
viet-tuan le <lvtuan@outlook.com>
2025-01-15 22:51:23 +0900

    Initial commit

--- scripts/write_md.py
+++ scripts/write_md.py
@@ -2,7 +2,7 @@ import json
 from datetime import datetime
 
 content = """
-# [Video Anomaly Detection](https://github.com/vt-le/Video-Anomaly-Detection) [![Awesome](https://cdn.rawgit.com/sindresorhus/awesome/d7305f38d29fed78fa85652e3a63e154dd8e8829/media/badge.svg)](https://github.com/sindresorhus/awesome)
+# [Awesomme Video Anomaly Detection](https://github.com/vt-le/Video-Anomaly-Detection) [![Awesome](https://cdn.rawgit.com/sindresorhus/awesome/d7305f38d29fed78fa85652e3a63e154dd8e8829/media/badge.svg)](https://github.com/sindresorhus/awesome)
 
 """
 
@@ -22,6 +22,43 @@ badges = {
 }
 
 data = json.loads(open("./scripts/data.json").read())
+
+# Convert the "Initial Date" from string to datetime object for accurate sorting
+for item in data:
+    item["Date"] = datetime.strptime(item["Date"], "%Y")
+
+# Sort the items by "Initial Date"
+data = sorted(data, key=lambda x: x["Date"])
+
+# Convert the "Initial Date" back to string format for displaying
+for item in data:
+    item["Date"] = item["Date"].strftime("%Y")
+
+for row in data:
+    content += f"| [{row['Title']}]({row['Link']}) | {row['Date']} | {row['Venue']} | "
+    # {row['Task']} | {row['Resource']} |\n
+    for task in row['Task']:
+        content += f"{badges[task.lower()]} "
+    content += f"| "
+    for k, v in row['Resource'].items():
+        if (v != "-"):
+            content += badges[k.lower()].format(v) + " "
+        else:
+            content += "-" + " "
+    content += f"| [{row['Model']}]({row['ModelLink']}) |\n"
+
+content += \
+"""
+🚀 🚀 🚀
+"""
+content += '''
+## Weakly Supervised VAD
+'''
+# write table headers
+content += "| Title | Date | Venue | Task | Resource | Model |\n"
+content += "| --- | --- | --- | --- | --- | --- |\n"
+
+data = json.loads(open("./scripts/data_weakly.json").read())
 
 # Convert the "Initial Date" from string to datetime object for accurate sorting
 for item in data:
